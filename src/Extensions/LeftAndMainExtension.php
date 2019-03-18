d9f1104b3adb7224026b3f1e5102cdd8b840acd7
Jaydn de Graaf <jd@pinc.nz>
2019-03-19 12:53:53 +1300

    Initial commit

--- src/Extensions/LeftAndMainExtension.php
+++ src/Extensions/LeftAndMainExtension.php
@@ -1,12 +1,1 @@
 <?php
-
-namespace permanentinc\beautiful\extensions;
-
-use SilverStripe\Core\Extension;
-use SilverStripe\View\Requirements;
-
-class LeftAndMainExtension extends Extension {
-    public function init() {
-        Requirements::css('permanentinc/beautiful:dist/styles/style.css');
-    }
-}
